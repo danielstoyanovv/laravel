88ef78e5466d6aa9f67495288995fbbc3c30b0d5
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2022-02-14 18:56:37 +0200

    Update routes - add new group and create define config for roles/users/flights Create permissions db table create permission config file Create seeder for creating all permissions Create seeder for creating new admin user with Admin role, which has all permissions

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Auth\FlightController as AuthFlightController;
 use App\Http\Controllers\Auth\FlightCrewController as AuthFlightCrewController;
 use App\Http\Controllers\Auth\PassengerController as AuthPassengerController;
 use App\Http\Controllers\FaceBook\CallbackController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,44 +29,36 @@ Route::get('/', function () {
 
 Route::get('/test/', [TestController::class, 'show']);
 
-Route::get('/flight/list', [FlightController::class, 'list'])->name('flights');
-
 Route::get('/flightcrew/list', [FlightCrewController::class, 'list'])->name('crews');
 
 Route::get('/passenger/list', [PassengerController::class, 'list'])->name('passengers');
 
 Auth::routes();
 
-Route::get('admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
-
-Route::get('/auth/flight/create', [AuthFlightController::class, 'create']);
-
-Route::post('/auth/flight/create', [AuthFlightController::class, 'create']);
-
-Route::get('/auth/flight/update/id/{id}', [AuthFlightController::class, 'update']);
-
-Route::post('/auth/flight/update/id/{id}', [AuthFlightController::class, 'update']);
-
-Route::get('/auth/flight/delete/id/{id}', [AuthFlightController::class, 'delete']);
-
-Route::get('/auth/flightcrew/create', [AuthFlightCrewController::class, 'create']);
-
-Route::post('/auth/flightcrew/create', [AuthFlightCrewController::class, 'create']);
-
-Route::get('/auth/flightcrew/update/id/{id}', [AuthFlightCrewController::class, 'update']);
-
-Route::post('/auth/flightcrew/update/id/{id}', [AuthFlightCrewController::class, 'update']);
-
-Route::get('/auth/flightcrew/delete/id/{id}', [AuthFlightCrewController::class, 'delete']);
-
-Route::get('/auth/passenger/create', [AuthPassengerController::class, 'create']);
-
-Route::post('/auth/passenger/create', [AuthPassengerController::class, 'create']);
-
-Route::get('/auth/passenger/update/id/{id}', [AuthPassengerController::class, 'update']);
-
-Route::post('/auth/passenger/update/id/{id}', [AuthPassengerController::class, 'update']);
-
 Route::get('facebook/redirect', [CallbackController::class, 'redirect']);
 
 Route::get('/facebook/callback', [CallbackController::class, 'login']);
+
+Route::group(['middleware' => ['auth']], function() {
+    /*
+    Route::get('/flights', [AuthFlightController::class, 'index'])->name('flights');
+    Route::get('/auth/flight/create', [AuthFlightController::class, 'create']);
+    Route::post('/auth/flight/create', [AuthFlightController::class, 'create']);
+    Route::get('/auth/flight/update/id/{id}', [AuthFlightController::class, 'update']);
+    Route::post('/auth/flight/update/id/{id}', [AuthFlightController::class, 'update']);
+    Route::get('/auth/flight/delete/id/{id}', [AuthFlightController::class, 'delete']);
+    */
+    Route::get('admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+    Route::get('/auth/flightcrew/create', [AuthFlightCrewController::class, 'create']);
+    Route::post('/auth/flightcrew/create', [AuthFlightCrewController::class, 'create']);
+    Route::get('/auth/flightcrew/update/id/{id}', [AuthFlightCrewController::class, 'update']);
+    Route::post('/auth/flightcrew/update/id/{id}', [AuthFlightCrewController::class, 'update']);
+    Route::get('/auth/flightcrew/delete/id/{id}', [AuthFlightCrewController::class, 'delete']);
+    Route::get('/auth/passenger/create', [AuthPassengerController::class, 'create']);
+    Route::post('/auth/passenger/create', [AuthPassengerController::class, 'create']);
+    Route::get('/auth/passenger/update/id/{id}', [AuthPassengerController::class, 'update']);
+    Route::post('/auth/passenger/update/id/{id}', [AuthPassengerController::class, 'update']);
+    Route::resource('flights', AuthFlightController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+});
