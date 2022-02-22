a09edc1ad9acf59cc26730b8f25f40306f404f70
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2022-02-22 20:01:06 +0200

    Update products template - add magento products custom attribute in list

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -13,6 +13,7 @@
             <th>{{ __('Type')}}</th>
             <th>{{ __('Created at')}}</th>
             <th>{{ __('Image')}}</th>
+            <th>{{ __('Custom attributes')}}</th>
         </tr>
         @foreach ($products['items'] as $product)
         <tr>        
@@ -25,11 +26,19 @@
             <td>{{ $product['visibility'] }}</td>
             <td>{{ $product['type_id'] }}</td>
             <td>{{ $product['created_at'] }}</td>
-            <td>{{ $product['attribute_set_id'] }}</td>
             <td>
                 @if (!empty($product['media_gallery_entries'][0]['file']))
                     <img src="http://magentolocal.com/pub/media/catalog/product{{ $product['media_gallery_entries'][0]['file'] }}" width="100px" />
                 @endif
+            </td>
+            <td>
+            @if (!empty($product['custom_attributes'])) 
+                @foreach ($product['custom_attributes'] as $attribute)
+                    @if (!is_array($attribute['value']))
+                        <p><b>{{ $attribute['attribute_code'] }}</b>: {{ $attribute['value'] }}</p>
+                    @endif
+                @endforeach
+            @endif
             </td>
         </tr>
         @endforeach 
