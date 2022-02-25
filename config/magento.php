f0d363a0c6971654c7c810c2d17e7aeec559201c
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2022-02-25 16:53:48 +0200

    Add url param in config file

--- config/magento.php
+++ config/magento.php
@@ -3,7 +3,7 @@
 return [
     "restapi_token" => "Bearer 9cbszji43911f54in77a4h80hppe43fr",
     "query_all_products" => "http://magentolocal.com/index.php/rest/V1/products?searchCriteria=0",
-    "create_update_product" => "http://magentolocal.com/index.php/rest/V1/products",
+    "create_update_product" => "http://magentolocal.com/index.php/rest/all/V1/products",
     "attribute_sets" => "http://magentolocal.com/index.php/rest/V1/products/attribute-sets/sets/list?searchCriteria=0",
     "currency_data" => "http://magentolocal.com/index.php/rest/V1/directory/currency"
 ];
