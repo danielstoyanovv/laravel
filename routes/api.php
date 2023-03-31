14fd84673e54cf6a9576c5316884bd559eebe184
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-03-31 16:41:53 +0300

    Remove FlightController.php update routes/api.php - declare new created users api Create API/UsersController.php - define restull methods

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\FlightController;
+use App\Http\Controllers\API\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //Route::apiResource('flights', FlightController::class)->middleware('jwt');
-Route::apiResource('flights', FlightController::class);
+Route::apiResource('users', UsersController::class);
+
