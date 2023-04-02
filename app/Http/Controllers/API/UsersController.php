82a7eb83960662b4b471ef6de418833df48cd206
Daniel Stoyanov <daniel.stoyanovv@gmail.com>
2023-04-02 18:14:03 +0300

    Update API/UsersController.php - remove create and validate processes - integrate new created service class logic Create UsersService.php - add create logic - add validate logic Create UsersApiTest.php - create api methods tests

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -8,10 +8,20 @@ use Illuminate\Http\Response;
 use App\Models\User;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
-use Illuminate\Support\Facades\Hash;
+use App\Http\Service\UsersService;
 
 class UsersController extends Controller
 {
+    /**
+     * @var UsersService
+     */
+    private $usersService;
+
+    public function __construct(UsersService $usersService)
+    {
+        $this->usersService = $usersService;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -29,8 +39,8 @@ class UsersController extends Controller
      */
     public function create(array $data)
     {
-        $this->validateApiCreateRequest($data);
-        if ($user = $this->createUser($data)) {
+        $this->usersService->validateApiCreateRequest($data);
+        if ($user = $this->usersService->createUser($data)) {
             return response()->json($user, Response::HTTP_CREATED);
         }
     }
@@ -99,7 +109,7 @@ class UsersController extends Controller
             if ($request->getMethod() == 'PATCH') {
                 if ($user = User::findOrFail($id)) {
                     if ($data = json_decode($request->getContent(), true)) {
-                        if ($user = $this->updateUser($data, $user)) {
+                        if ($user = $this->usersService->updateUser($data, $user)) {
                             return response()->json($user);
                         }
                     }
@@ -120,100 +130,5 @@ class UsersController extends Controller
     public function destroy($id)
     {
         //
-    }
-
-    /**
-     * @param array $data
-     * @return void
-     */
-    private function validateApiCreateRequest(array $data)
-    {
-        if (empty($data['name'])) {
-            throw new UnprocessableEntityHttpException("'name' is required field");
-        }
-
-        if (empty($data['email'])) {
-            throw new UnprocessableEntityHttpException("'email' is required field");
-        }
-        $this->validateEmail($data['email']);
-
-        $this->checkIfEmailExists($data['email']);
-
-        if (empty($data['password'])) {
-            throw new UnprocessableEntityHttpException("'password' is required field");
-        }
-
-        $this->validatePassword($data['password']);
-    }
-
-    private function validatePassword(string $password)
-    {
-        if (strlen($password) < 6) {
-            throw new UnprocessableEntityHttpException("'password' minimum length is 6 characters");
-        }
-    }
-
-    /**
-     * @param string $email
-     * @return void
-     */
-    private function validateEmail(string $email)
-    {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new UnprocessableEntityHttpException("'email' is not valid");
-        }
-    }
-
-    /**
-     * @param string $email
-     * @return void
-     */
-    private function checkIfEmailExists(string $email)
-    {
-        if ($user = User::where('email', $email)-> first()) {
-            throw new UnprocessableEntityHttpException(
-                sprintf("'%s' already exists", $email)
-            );
-        }
-    }
-
-    /**
-     * @param array $data
-     * @return User
-     */
-    private function createUser(array $data): User
-    {
-        $user = User::create(array(
-            'name' => $data['name'],
-            'password' => Hash::make($data['password']),
-            'email' => $data['email']
-        ));
-
-        return $user;
-    }
-
-    /**
-     * @param array $data
-     * @param User $user
-     * @return User
-     */
-    private function updateUser(array $data, User $user): User
-    {
-        if (!empty($data['name']) && $user->getAttributes()['name'] != $data['name']) {
-            $user->update(['name' => $data['name']]);
-        }
-
-        if (!empty($data['email']) && $user->getAttributes()['email'] != $data['email']) {
-            $this->validateEmail($data['email']);
-            $this->checkIfEmailExists($data['email']);
-            $user->update(['email' => $data['email']]);
-        }
-
-        if (!empty($data['password']) && $user->getAttributes()['password'] != Hash::make($data['password'])) {
-            $this->validatePassword($data['password']);
-            $user->update(['password' => Hash::make($data['password'])]);
-        }
-
-        return $user;
     }
 }
